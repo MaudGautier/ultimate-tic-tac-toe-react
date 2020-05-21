e9ef8846cda934891d039927807d981c481de6d3
Maud Gautier <maud.j.gautier@gmail.com>
2020-05-21 12:03:30 +0200

    Determine when whole game won and stop game

--- src/Game.js
+++ src/Game.js
@@ -12,6 +12,7 @@ class Game extends React.Component {
 			players: ["X", "O"],
 			bigBoard: null,
 			boardsWinners: null, // Array of 3x3 winner values (each smallBoard)
+			gameWinner: null,
 		};
 	}
 
@@ -92,7 +93,9 @@ class Game extends React.Component {
 		const smallBoard = board[boardRow][boardCol]
 
 		// Return early if game already won
-		if (this)
+		if (this.state.gameWinner) {
+			return;
+		}
 
 		// Return early if move invalid
 		if (this.state.boardsWinners[boardRow][boardCol] || smallBoard[cellRow][cellCol]) {
@@ -102,25 +105,23 @@ class Game extends React.Component {
 
 		// Change player turn
 		board[boardRow][boardCol][cellRow][cellCol] = this.state.playerTurn;
-		this.setState({
-			bigBoard: board,
-			playerTurn: this.state.playerTurn === "X" ? "O" : "X",
-		});
-
+		
 
 		// Determine if smallboard won
 		var winner = getWinner(smallBoard);
-		if (winner) {
-			const boardsWinners = JSON.parse(JSON.stringify(this.state.boardsWinners));
-			boardsWinners[boardRow][boardCol] = winner;
-			this.setState({
-				boardsWinners: boardsWinners,
-			})
-		}
+		const boardsWinners = JSON.parse(JSON.stringify(this.state.boardsWinners));
+		boardsWinners[boardRow][boardCol] = winner;
 
 		// Determine if game won
-		var gameWinner = getWinner(this.state.boardsWinners)
+		var gameWinner = getWinner(boardsWinners)
 
+		// Update board display
+		this.setState({
+			bigBoard: board,
+			playerTurn: this.state.playerTurn === "X" ? "O" : "X",
+			boardsWinners: boardsWinners,
+			gameWinner: gameWinner,
+		});
 
 
 	}
